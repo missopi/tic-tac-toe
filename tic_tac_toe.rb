6c9450d9fb18b6097ddb9ef0c3b3a9f13be4f65c
missopi <missopi@me.com>
2022-09-27 21:46:41 +0100

    Add method for displaying current board

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -13,6 +13,10 @@ class Board
     puts '_________'
     puts "#{board[6]} | #{board[7]} | #{board[8]}"
   end
+
+  def current_board(board)
+    display_board(board)
+  end
 end
 
 # class for assigning players
@@ -27,5 +31,6 @@ end
 
 # tic tac toe game class
 class Game
+  
   [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [6, 4, 2], [0, 4, 8]]
 end
