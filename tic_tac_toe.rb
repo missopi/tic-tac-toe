0d0ccbc9c9e4797efa8e6081aee8b88127030825
missopi <missopi@me.com>
2022-09-27 15:30:37 +0100

    Remove arrays so board rows are horizontal

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -5,11 +5,11 @@ class Board
   end
 
   def display_board(board)
-    puts row_1 = ['#{board[0]}','|', '#{board[1]}', '|', '#{board[2]}']
-    puts spacer_row = ['_____']
-    puts row_2 = ['#{board[3]}','|', '#{board[4]}', '|', '#{board[5]}']
+    puts row_1 = "#{board[0]}", '|', "#{board[1]}", '|', "#{board[2]}"
+    puts spacer_row = '_____'
+    puts row_2 = "#{board[3]}", '|', "#{board[4]}", '|', "#{board[5]}"
     puts spacer_row
-    puts row_3 = ['#{board[6]}','|', '#{board[7]}', '|', '#{board[8]}']
+    puts row_3 = "#{board[6]}", '|', "#{board[7]}", '|', "#{board[8]}"
   end
 end
 
