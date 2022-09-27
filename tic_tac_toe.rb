48590a39c7d2e8997a6155754f652ceb214a6765
missopi <missopi@me.com>
2022-09-27 15:48:51 +0100

    Add constant in board class for winning combos

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -11,6 +11,9 @@ class Board
     puts '_________'
     puts "#{board[6]} | #{board[7]} | #{board[8]}"
   end
+
+  WIN_COMBOS = [[0, 1, 2], [3, 4, 5],[ 6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [6, 4, 2], [0, 4, 8]]
+  
 end
 
 class Player
