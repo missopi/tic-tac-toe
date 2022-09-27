41547436abe25fb77e02d57935627ad973bfbddc
missopi <missopi@me.com>
2022-09-27 15:35:00 +0100

    Remove quotations and commas - not needed

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -5,11 +5,11 @@ class Board
   end
 
   def display_board(board)
-    puts "#{board[0]}", '|', "#{board[1]}", '|', "#{board[2]}"
-    puts '_______'
-    puts "#{board[3]}", '|', "#{board[4]}", '|', "#{board[5]}"
-    puts '_______'
-    puts "#{board[6]}", '|', "#{board[7]}", '|', "#{board[8]}"
+    puts "#{board[0]} | #{board[1]} | #{board[2]}"
+    puts '_________'
+    puts "#{board[3]} | #{board[4]} | #{board[5]}"
+    puts '_________'
+    puts "#{board[6]} | #{board[7]} | #{board[8]}"
   end
 end
 
