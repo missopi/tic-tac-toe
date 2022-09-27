aeb5056977520cd4fab6bfb6085730d80cbb8c10
missopi <missopi@me.com>
2022-09-27 22:20:30 +0100

    Add turn method code

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -41,7 +41,16 @@ class Game
 
   def current_player(board) end
 
-  def turn(board) end
+  def turn(board)
+    puts 'Please make your choice of move from 1-9'
+    input = gets.chomp.to_i
+    if (input - 1) == valid_move(index)
+      move(board, index, token)
+      current_board(board)
+    else
+      puts 'Please choose a valid number as your move'
+    end
+  end
 
   def win?(board)
     [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [6, 4, 2], [0, 4, 8]] # winning combos
