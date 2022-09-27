f9ca359ab217e2698f07fae9bfe2da0ad3545c7a
missopi <missopi@me.com>
2022-09-27 21:48:16 +0100

    Add move method

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -31,6 +31,7 @@ end
 
 # tic tac toe game class
 class Game
-  
+  def move(board, index, token) end
+
   [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [6, 4, 2], [0, 4, 8]]
 end
