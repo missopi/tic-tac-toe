0c8a8e9a137a9f16097794ad18d7f02cece66610
missopi <missopi@me.com>
2022-09-27 15:15:06 +0100

    Define board display

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -4,5 +4,12 @@ class Board
     @board = ['','','','','','','','','']
   end
 
+  def display_board(board)
+    puts row = ['#{board[0]}','|', '#{board[1]}', '|', '#{board[2]}']
+    puts spacer_row = ['_____']
+    puts row
+    puts spacer_row
+    puts row
+  end
 end
   
