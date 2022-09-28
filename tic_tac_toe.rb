f73c05a965a9b1a456ed954069bc1cb4824b243a
missopi <missopi@me.com>
2022-09-28 14:35:44 +0100

    Remove unnecessary code

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -49,18 +49,18 @@ class Game
 
   def current_player(board) end
 
-  def turn(board)
+  def turn
     puts 'Please make your choice of move from 1-9'
     input = gets.chomp.to_i
     if (input - 1) == valid_move(index)
-      move(board, index, token)
-      current_board(board)
+      move
+      current_board
     else
       puts 'Please choose a valid number as your move'
     end
   end
 
-  def win?(_board)
+  def win?
     [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [6, 4, 2], [0, 4, 8]] # winning combos
   end
 
@@ -72,8 +72,8 @@ class Game
 
   def winner(board) end
 
-  def play(board)
-    move(board, index, token) until over == true
+  def play
+    move until over == true
     if win == true
       puts "Congratulations #{player}"
     else
