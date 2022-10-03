6234aab1486b84395e05dab75d03ff25c4ec5e14
missopi <missopi@me.com>
2022-10-03 21:46:53 +0100

    Add extra spaces to inside of display board

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -39,11 +39,11 @@ class Board
 
   def display_board(board)
     puts ' '
-    puts "#{board[0]} | #{board[1]} | #{board[2]}"
-    puts '_________'
-    puts "#{board[3]} | #{board[4]} | #{board[5]}"
-    puts '_________'
-    puts "#{board[6]} | #{board[7]} | #{board[8]}"
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts '___________'
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts '___________'
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
     puts ' '
   end
 end
