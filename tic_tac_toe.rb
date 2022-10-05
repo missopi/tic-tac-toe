2cd90ac7a5e3bc450e5f242cbcf145ce6ed4fcd5
missopi <missopi@me.com>
2022-10-05 20:09:56 +0100

    Simplify win method
    Add * to deconstruct array to 3 seperate values

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -96,10 +96,8 @@ class Game
 
   def win
     WIN_COMBOS.each do |line| # check if line has all three tokens the same
-      if (@board.board[line[0]] == @board.board[line[1]] && @board.board[line[1]] == @board.board[line[2]]) &&
-         @board.board[line[0]] != ' '
-        winner
-      end
+      syms = @board.board.values_at(*line).uniq
+      winner if syms.length == 1 && syms[0] != ' '
     end
   end
 
