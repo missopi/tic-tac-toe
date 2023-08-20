4fe00181f29c2ac4cd1b6f2236efe60852d74c3e
missopi <missopi@me.com>
2023-08-20 17:29:33 +0100

    List methods to be tested from tic_tac_toe file

---
+++ spec/tic_tac_toe_spec.rb
@@ -0,0 +1,52 @@
+require './lib/tic_tac_toe'
+
+describe Player do
+  describe '#initialize' do
+  end
+end
+
+describe Board do
+  describe '#initialize' do
+  end
+
+  describe '#update_board' do
+  end
+
+  describe 'display_board' do
+  end
+end
+
+describe Game do
+  describe '#initialize' do
+  end
+
+  describe '#space_not_taken?' do
+  end
+
+  describe '#valid_move?' do
+  end
+
+  describe 'current_player' do
+  end
+
+  describe '#move' do
+  end
+
+  describe '#player_turn' do
+  end
+
+  descibe '#win' do
+  end
+
+  describe '#winner' do
+  end
+
+  descibe '#draw' do
+  end
+
+  describe 'over' do
+  end
+
+  describe 'play' do
+  end
+end
